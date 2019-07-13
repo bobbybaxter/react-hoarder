bde07e99a1233af0a4e22db33f00034a5e84a39a
Bob <bob.e.baxter@gmail.com>
2019-07-13 13:22:01 -0500

    read works

--- src/components/SingleItem/SingleItem.js
+++ src/components/SingleItem/SingleItem.js
@@ -1,16 +1,41 @@
+/* eslint-disable max-len */
 import React from 'react';
 // import firebase from 'firebase/app';
 // import 'firebase/auth';
 
-// import itemData from '../../helpers/data/itemData';
+import stuffData from '../../helpers/data/stuffData';
 
 import './SingleItem.scss';
 
 class SingleItem extends React.Component {
+  state = {
+    item: {},
+  }
+
+  componentDidMount() {
+    const itemId = this.props.match.params.id;
+    stuffData.getSingleItem(itemId)
+      .then(itemPromise => this.setState({ item: itemPromise.data }))
+      .catch(err => console.error(err));
+  }
+
   render() {
+    const { item } = this.state;
     return (
       <div className="SingleItem">
-        <h1>Single Item</h1>
+        <div className="ItemCard mb-3">
+          <div className="card border-0 shadow">
+            <div className="card-body">
+              <h5 className="card-title">{item.itemName}</h5>
+              <img src={item.itemImage} alt={item.itemName} className="stuffImg"></img>
+              <p className="card-text">{item.itemlocation}</p>
+              <div>
+                {/* <Link href="#" className="btn btn-outline-primary" to={editLink}>Edit</Link> */}
+                {/* <button href="#" className="btn btn-outline-danger" onClick={this.deleteMe}>Delete</button> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/helpers/data/stuffData.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import fbKeys from '../apiKeys.json';
+
+const baseUrl = fbKeys.firebaseKeys.databaseURL;
+
+const getStuff = uid => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/stuff.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((res) => {
+      const items = [];
+      console.error(items);
+      if (res.data !== null) {
+        Object.keys(res.data).forEach((fbKey) => {
+          res.data[fbKey].id = fbKey;
+          items.push(res.data[fbKey]);
+        });
+      }
+      resolve(items);
+    })
+    .catch(err => reject(err));
+});
+
+const getSingleItem = itemId => axios.get(`${baseUrl}/stuff/${itemId}.json`);
+
+export default { getStuff, getSingleItem };
